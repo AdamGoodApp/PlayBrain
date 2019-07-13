7a951fb6f207e22db6f8785fec24fe53c5e17d7c
adamgoodapp <adamgoodapp@gmail.com>
2019-07-13 19:33:26 +0900

    Add material ui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "typeface-roboto";
 
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <Router>
+      <header>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </header>
       <div>{/* <Route path="/" exact component={} /> */}</div>
     </Router>
   );
