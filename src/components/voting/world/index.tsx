23066702163bac38aa421ac13add188b241d0a63
adamgoodapp <adamgoodapp@gmail.com>
2019-07-14 16:38:20 +0900

    Add more styling to world selection and cards

--- src/components/voting/world/index.tsx
+++ src/components/voting/world/index.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Typography from "@material-ui/core/Typography";
 import Card from "../../cards";
 
 import "./index.scss";
@@ -7,6 +8,10 @@ class World extends Component<any, any> {
   render() {
     return (
       <div className="world-container">
+        <Typography variant="h2" gutterBottom style={{ textAlign: "center" }}>
+          Select Region
+        </Typography>
+
         <Card />
       </div>
     );
